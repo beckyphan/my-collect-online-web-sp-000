6193110860364d5b298b2e83f5a44c4e4c31d08e
Becky Phan <beckyphan@live.com>
2019-05-29 02:26:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,13 @@
 def my_collect(collection)
+  i = 0
+  while i < collection.length 
+    yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
 
+collection = [1, 2, 3, 4]
+my_collect(collection) do |i|
+  print i
 end
